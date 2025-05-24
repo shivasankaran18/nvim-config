1c5165eb7f6bd1f52ea067b9d0e66e7ce67292bd
shiva <shiva_05@outlook.com>
2025-05-24 07:49:13 +0530

    revamp:key bindings

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -7,6 +7,20 @@ return {
 
   },
   {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+
+   
+  {
     "L3MON4D3/LuaSnip",
     dependencies = {
       "saadparwaiz1/cmp_luasnip",
--- lua/plugins/gits.lua
+++ lua/plugins/gits.lua
@@ -10,41 +10,50 @@ return {
 	{
 		"lewis6991/gitsigns.nvim",
 		config = function()
-			require("gitsigns").setup({
-				current_line_blame = true, -- Show blame for the current line
-				current_line_blame_opts = {
-					virt_text = true,
-					virt_text_pos = "eol", -- Place blame at the end of the line
-					delay = 500,
-					ignore_whitespace = false,
-				},
-				current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
-				sign_priority = 6, -- Priority of git signs
-				update_debounce = 100,
-				preview_config = {
-					border = "rounded",
-				},
-			})
-
-			-- Key mappings for GitSigns
-			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
-
-			-- Define highlights
-			vim.api.nvim_set_hl(0, "GitSignsAdd", { fg = "#A6E22E" }) -- Green for additions
-			vim.api.nvim_set_hl(0, "GitSignsChange", { fg = "#FD971F" }) -- Yellow for changes
-			vim.api.nvim_set_hl(0, "GitSignsDelete", { fg = "#F92672" }) -- Red for deletions
-
-			vim.api.nvim_set_hl(0, "GitSignsAddNr", { link = "GitSignsAdd" })
-			vim.api.nvim_set_hl(0, "GitSignsChangeNr", { link = "GitSignsChange" })
-			vim.api.nvim_set_hl(0, "GitSignsDeleteNr", { link = "GitSignsDelete" })
-
-			vim.api.nvim_set_hl(0, "GitSignsAddLn", { link = "GitSignsAdd" })
-			vim.api.nvim_set_hl(0, "GitSignsChangeLn", { link = "GitSignsChange" })
-			vim.api.nvim_set_hl(0, "GitSignsDeleteLn", { link = "GitSignsDelete" })
-
-			vim.api.nvim_set_hl(0, "GitSignsTopdelete", { link = "GitSignsDelete" })
-			vim.api.nvim_set_hl(0, "GitSignsChangedelete", { link = "GitSignsChange" })
+			require('gitsigns').setup {
+  signs = {
+    add          = { text = '┃' },
+    change       = { text = '┃' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '~' },
+    untracked    = { text = '┆' },
+  },
+  signs_staged = {
+    add          = { text = '┃' },
+    change       = { text = '┃' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '~' },
+    untracked    = { text = '┆' },
+  },
+  signs_staged_enable = true,
+  signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+  numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
+  linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+  word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
+  watch_gitdir = {
+    follow_files = true
+  },
+  auto_attach = true,
+  attach_to_untracked = false,
+  current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+    delay = 500,
+    ignore_whitespace = false,
+  },
+  current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+  sign_priority = 6,
+  update_debounce = 100,
+  status_formatter = nil, -- Use default
+  max_file_length = 40000, -- Disable if file is longer than this (in lines)
+  preview_config = {
+    -- Options passed to nvim_open_win
+      border = 'rounded', -- 'single', 'double', 'shadow', 'rounded'
+  },
+}
 		end,
 	},
 }
